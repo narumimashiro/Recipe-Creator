8803af1815e623d3314a923819891970b0212677
Narumi_Mashiro <narunarumashiro96@gmail.com>
2023-11-08 18:54:21 +0900

    CSSの調整 背景に画像を設定 APIの呼び出し、形だけ追加

--- src/components/molecules/RecipeBoard.tsx
+++ src/components/molecules/RecipeBoard.tsx
@@ -1,12 +1,27 @@
-
 import styles from '@/styles/components/RecipeBoard.module.sass'
 
-const RecipeBoard = () : JSX.Element => {
+// MaterialUI
+import TextField from '@mui/material/TextField'
+
+// Recoil
+import { useRecoilValue } from 'recoil'
+import { Recipe } from '@/recoil/Recipe'
+
+const RecipeBoard = (): JSX.Element => {
+  
+  const recipe = useRecoilValue(Recipe)
 
   return (
     <>
       <div className={styles.container}>
-
+        <TextField
+          className={styles['recipe-output']}
+          multiline
+          minRows={25}
+          maxRows={30}
+          variant='outlined'
+          value={recipe}
+        />
       </div>
     </>
   )
