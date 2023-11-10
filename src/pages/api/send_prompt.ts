0ca0f3309592af4ae04ab647119eeb3079e210ba
Narumi_Mashiro <narunarumashiro96@gmail.com>
2023-11-11 00:41:21 +0900

    openai apikey fix

--- src/pages/api/send_prompt.ts
+++ src/pages/api/send_prompt.ts
@@ -3,8 +3,8 @@
  */
 
 import type { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios'
 import OpenAI, { ClientOptions } from 'openai'
+import Configuration from 'openai'
 
 const handler = async (
   req: NextApiRequest,
@@ -17,18 +17,22 @@ const handler = async (
     res.status(200).json('Sorry, u cannot use this service now')
   }
 
-  const apiKey = process.env.OPENAI_API_KEY
+  const apiKey = process.env.OPENAI_APIKEY
 
   if (!apiKey) {
     res.status(500).json('API key not configured')
   } else {
-    const openai = new OpenAI({ key: apiKey } as ClientOptions)
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [{"role": "user", "content": prompt}],
-    })
-    
-    res.status(200).json(response.choices[0].message.content)
+    const openai = new OpenAI({ apiKey: apiKey })
+    try {
+      const response = await openai.chat.completions.create({
+        messages: [{ "role": "user", "content": prompt }],
+        model: "gpt-3.5-turbo",
+      })
+      res.status(200).json(response.choices[0].message.content)
+    }
+    catch(err) {
+      res.status(500).json('cannot response chatGPT : ' + err)
+    }
   }
 }
 export default handler
