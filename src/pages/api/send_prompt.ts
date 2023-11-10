80278be41d1a594e7947aad994242508ab979a66
Narumi_Mashiro <narunarumashiro96@gmail.com>
2023-11-10 15:38:47 +0900

    api key fix

--- src/pages/api/send_prompt.ts
+++ src/pages/api/send_prompt.ts
@@ -4,7 +4,7 @@
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
-import OpenAI from 'openai'
+import OpenAI, { ClientOptions } from 'openai'
 
 const handler = async (
   req: NextApiRequest,
@@ -13,16 +13,22 @@ const handler = async (
 
   const { prompt } = req.body
 
-  if (process.env.PROD_ENV == 'false') {
+  if (process.env.PROD_ENV == process.env.CONST_API_STOP) {
     res.status(200).json('Sorry, u cannot use this service now')
   }
 
-  const openai = new OpenAI()
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    messages: [{"role": "user", "content": prompt}],
-  })
-  
-  res.status(200).json(response.choices[0].message.content)
+  const apiKey = process.env.OPENAI_API_KEY
+
+  if (!apiKey) {
+    res.status(500).json('API key not configured')
+  } else {
+    const openai = new OpenAI({ key: apiKey } as ClientOptions)
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{"role": "user", "content": prompt}],
+    })
+    
+    res.status(200).json(response.choices[0].message.content)
+  }
 }
 export default handler
